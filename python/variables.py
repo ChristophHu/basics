b34212fc410f8e5e82cc9ae8d801c84e8579e1d5
christophhu <christoph.huschenhoefer@gmx.de>
2017-10-23 15:21:26 +0200

    add python > basics

---
+++ python/variables.py
@@ -0,0 +1,33 @@
+# python basics
+
+# -------------------------
+# <<< terminal commads >>>
+# touch pythonfile.py
+# code pythonfile.py
+
+# -------------------------
+# first code/program
+print("Hello World")
+
+# -------------------------
+# variable declaration
+stringVar = "Hello World"
+integerVar = 1
+floatVar = 9.0
+
+# -------------------------
+# types
+integerVar = Int(floatVar)
+stringVar = str(floatar)
+print(type(integerVar))
+boolVar = int(floatVar) == integerVar   # compare
+
+# -------------------------
+# arithmetic operations
+integerVar = floatVar   # equal
+integerVar += floatVar  # addition
+integerVar -= floatVar  # subtraction
+integer += floatVar     # multiplication
+integer /= floatVar     # division
+integer %= floatVar     # modulo
+
