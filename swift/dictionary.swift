d86d2c2417f3910eaf144a7ddd23c6c37c140147
christophhu <christoph.huschenhoefer@gmx.de>
2017-10-24 11:52:05 +0200

    add swift/dictionary.swift

---
+++ swift/dictionary.swift
@@ -0,0 +1,11 @@
+// Array
+var array = ["index_0", "index_1", "index_2"]
+print(array[0])
+
+// Dictionary
+var dict : [String : String] = ["key": "value"]
+print(dict.count)
+print(dict["key"]!)
+
+let value : String! = dict["key"]
+print(value)
